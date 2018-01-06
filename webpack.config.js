089272ef3df822f601298f1f03bc730bc8700767
kcirekcom <erick.f.mock@gmail.com>
2018-01-06 15:33:54 -0800

    had to add another dependency, will most likely remove other files later on

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 
 const webpack = require('webpack');
 const HTMLPlugin = require('html-webpack-plugin');
-const CleanPlugin = require('clean-webpack-plugin');
+// const CleanPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 let plugins = [
