7fd245c6f23d11eb45da0a0e966e9a119cb8b80a
kcirekcom <erick.f.mock@gmail.com>
2018-01-06 17:35:52 -0800

    built Header component to fetch artist info from API based on URI input

---
+++ src/components/Header.jsx
@@ -0,0 +1,46 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    };
+  }
+
+  fetchHeaderInfo = () => {
+    var artist = encodeURIComponent(this.props.artist);
+
+    fetch(`https://rest.bandsintown.com/artists/${artist}?app_id=bit_challenge`).then(response => {
+      return response.json();
+    }).then(data => {
+      console.log(data);
+      this.setState({
+        data: data
+      });
+    }).catch(err => {
+      console.error(err);
+    });
+  }
+
+  componentWillMount() {
+    this.fetchHeaderInfo();
+  }
+
+  render() {
+    if (this.state.data == null) {
+      return (
+        <div/>
+      )
+    }
+
+    return (
+      <div>
+        <span>{this.state.data.name}</span>
+        <img src={this.state.data.thumb_url}/>
+      </div>
+    );
+  }
+}
